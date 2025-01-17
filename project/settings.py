b91883c3c0650a3933339eb84bc7765a87013f76
NourElDienNwara <noureldiennwara312@gmail.com>
2025-01-17 13:15:06 +0200

    admin

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'Static']
 
+STATIC_ROOT = 'staticfiles'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
