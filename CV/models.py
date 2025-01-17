69e135848f3e44a3a751848a9ef24b0a40ac4431
NourElDienNwara <noureldiennwara312@gmail.com>
2025-01-17 17:04:12 +0200

    admin

--- CV/models.py
+++ CV/models.py
@@ -41,10 +41,10 @@ class Skills(models.Model):
     skill = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return self.skill
 
     def __unicode__(self):
-        return self.name
+        return self.skill
 
 
 class Certificate(models.Model):
